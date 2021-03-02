08a9ffc23435610bd3180a77ccc98a9c3d6e9c18
Nirajan995 <52143467+Nirajan995@users.noreply.github.com>
2021-03-02 21:14:35 +0545

    Update User.js

--- react-frontend/src/components/User.js
+++ react-frontend/src/components/User.js
@@ -1,29 +1,12 @@
-import React,{useState} from 'react'
-import "./User.css"
-import { Link } from 'react-router-dom';
+import React from "react";
+import Userdashboard from "./Userdashboard";
 
 function User() {
-    let time = new Date().toLocaleTimeString();
-    let date = new Date().toLocaleDateString();
-      const [ctime,setCtime]=useState(time);
-      const [tdate,setTdate]=useState(date);
-  
-      const UpdateTime=()=>{
-          time = new Date().toLocaleTimeString();
-          setCtime(time);
-          date = new Date().toLocaleDateString();
-          setTdate(date);
-      }
-      setInterval(UpdateTime,1000)
-      return (
-      <div className="mainpage">
-          
-              <span className="welcome">Welcome to KANN HRMS</span>
-          
-      </div>
-      
-      )
-
+  return (
+    <div>
+      <Userdashboard />
+    </div>
+  );
 }
 
-export default User
+export default User;
